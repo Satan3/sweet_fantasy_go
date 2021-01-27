3fcf9e609843435f87f0a076fb49d43479b10ab5
Igor Emelyanov <mega.itachi.uchiha@mail.ru>
2021-01-27 21:52:23 +0300

    Main.go

---
+++ internal/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gofiber/fiber"
+	"github.com/joho/godotenv"
+	"sweet_fantasy_go/internal/database"
+	"sweet_fantasy_go/internal/handlers"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		panic("There is no .env file")
+	}
+}
+
+func main() {
+	app := fiber.New()
+	setupRoutes(app)
+	database.InitDatabase()
+
+	app.Listen(":3000")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Static("/files", "./assets")
+
+	group := app.Group("/categories")
+	group.Get("/list", handlers.GetCategories)
+	group.Post("/create", handlers.CreateCategory)
+}
