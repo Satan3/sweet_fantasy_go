e4e38c1159253d418fc36f4969910209d7740ba8
1Satan3 <mega.itachi.uchiha@mail.ru>
2021-02-04 22:50:57 +0300

    Service for working with files

--- internal/services/fileService.go
+++ internal/services/fileService.go
@@ -1,27 +1,50 @@
 package services
 
 import (
+	"crypto/sha1"
 	"fmt"
 	"github.com/valyala/fasthttp"
 	"mime/multipart"
 	"path/filepath"
+	"strings"
+	db "sweet_fantasy_go/internal/database"
 	"sweet_fantasy_go/internal/models"
+	"time"
 )
 
 func CreateAndSaveFile(file *multipart.FileHeader, filePath string) (*models.File, error) {
-	relativePath := getRelativePath(file.Filename, filePath)
-
-	fullPath, err := getFullPath(relativePath)
+	relativePath, err := saveFile(file, filePath)
 	if err != nil {
 		return nil, err
+	}
+	return &models.File{Path: relativePath}, nil
+}
+
+func ReplaceFile(newFile *multipart.FileHeader, filePath string, prevFile *models.File) error {
+	relativePath, err := saveFile(newFile, filePath)
+	if err != nil {
+		return err
+	}
+	if err = prevFile.RemoveFromStorage(); err != nil {
+		return err
+	}
+	prevFile.Path = relativePath
+	db.DBConn.Save(&prevFile)
+	return nil
+}
+
+func saveFile(file *multipart.FileHeader, filePath string) (relative string, err error) {
+	relativePath := getRelativePath(generateFileName(file), filePath)
+	fullPath, err := getFullPath(relativePath)
+	if err != nil {
+		return "", err
 	}
 
 	err = fasthttp.SaveMultipartFile(file, fullPath)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-
-	return &models.File{Path: relativePath}, nil
+	return relativePath, err
 }
 
 func getRelativePath(fileName string, filePath string) string {
@@ -38,4 +61,14 @@ func getFullPath(relativePath string) (string, error) {
 		"../assets/%s",
 		relativePath,
 	))
+}
+
+func generateFileName(file *multipart.FileHeader) string {
+	separatedString := strings.Split(file.Filename, ".")
+	return strings.Join([]string{generateTimestampHash(), separatedString[len(separatedString)-1]}, ".")
+}
+
+func generateTimestampHash() string {
+	timestampBytes := []byte(time.Now().Format("2006-01-02 15:04:05"))
+	return fmt.Sprintf("%x", sha1.Sum(timestampBytes))
 }
