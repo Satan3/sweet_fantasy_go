795b7f879868858ae0c6411b4fef63a71abb75d3
1Satan3 <mega.itachi.uchiha@mail.ru>
2021-02-04 11:34:23 +0300

    Validation method in validation package

--- internal/validation/validation.go
+++ internal/validation/validation.go
@@ -1,7 +1,25 @@
 package validation
 
+import "github.com/go-playground/validator"
+
 type Error struct {
 	FailedField string
 	Tag         string
 	Value       string
+}
+
+func Validate(model interface{}) []*Error {
+	var errors []*Error
+	validate := validator.New()
+	err := validate.Struct(model)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			element := Error{
+				FailedField: err.Field(),
+				Tag:         err.Tag(),
+			}
+			errors = append(errors, &element)
+		}
+	}
+	return errors
 }
