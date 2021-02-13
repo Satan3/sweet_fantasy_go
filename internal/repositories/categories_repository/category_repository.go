3b790877386382af3275cdcbfc34a0c473ff6029
1Satan3 <mega.itachi.uchiha@mail.ru>
2021-02-13 14:23:21 +0300

    Category Repository

---
+++ internal/repositories/categories_repository/category_repository.go
@@ -0,0 +1,31 @@
+package categories_repository
+
+import (
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
+	db "sweet_fantasy_go/internal/database"
+	"sweet_fantasy_go/internal/models"
+)
+
+func FindAll() (categories []models.Category) {
+	db.DBConn.Joins("File").Find(&categories)
+	return
+}
+
+func FindById(id string) (*models.Category, error) {
+	category := new(models.Category)
+	result := db.DBConn.Joins("File").First(&category, id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, errors.New(fmt.Sprintf("Не существует категории с Id: %s", id))
+	}
+	return category, nil
+}
+
+func Save(category *models.Category) {
+	db.DBConn.Save(&category)
+}
+
+func Delete(category *models.Category) {
+	db.DBConn.Delete(&category)
+}
