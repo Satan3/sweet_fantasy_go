d74a64ff51a864dd93e0a12721fb5bb9e3881a4d
1Satan3 <mega.itachi.uchiha@mail.ru>
2021-02-13 14:21:14 +0300

    Added Product model

---
+++ internal/models/product.go
@@ -0,0 +1,20 @@
+package models
+
+const ProductPath = "products"
+
+type Product struct {
+	Base
+	Name        string `validate:"required" json:"name"`
+	Title       string `validate:"required" json:"title"`
+	Description string `validate:"required" json:"description"`
+	Keywords    string `validate:"required" json:"keywords"`
+	Price       int    `validate:"required,numeric" json:"price"`
+	Discount    int    `json:"discount"`
+	HitSales    bool   `json:"hit_sales"`
+
+	CategoryId uint `json:"category_id"`
+	Category   `json:"category" gorm:"constraint:OnDelete:SET NULL"`
+
+	FileId uint `json:"-"`
+	File   File `json:"file" gorm:"constraint:OnDelete:SET NULL"`
+}
