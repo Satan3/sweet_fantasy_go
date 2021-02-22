e7132cac3ede9f1c6918a4c8d8492fffad192e60
1Satan3 <mega.itachi.uchiha@mail.ru>
2021-02-22 16:30:06 +0300

    Category update and create refactoring

--- internal/handlers/categories.go
+++ internal/handlers/categories.go
@@ -3,103 +3,87 @@ package handlers
 import (
 	"github.com/gofiber/fiber/v2"
 	"net/http"
+	"strconv"
 	"sweet_fantasy_go/internal/models"
 	categoriesRepository "sweet_fantasy_go/internal/repositories/categories_repository"
-	"sweet_fantasy_go/internal/services"
+	filesRepository "sweet_fantasy_go/internal/repositories/files_repository"
 	"sweet_fantasy_go/internal/validation"
 )
 
 func GetCategories(ctx *fiber.Ctx) error {
-	return ctx.JSON(categoriesRepository.FindAll())
+	return successResponse(ctx, categoriesRepository.FindAll())
 }
 
 func CreateCategory(ctx *fiber.Ctx) error {
 	category := new(models.Category)
 
 	if err := ctx.BodyParser(category); err != nil {
-		return ctx.Status(http.StatusBadRequest).JSON(err)
+		return errorResponse(ctx.Status(http.StatusBadRequest), err.Error())
 	}
 
-	image, err := ctx.FormFile("image")
-	if err != nil {
-		return ctx.JSON(fiber.Map{
-			"success": false,
-			"message": "Отсутствует файл обложки",
-		})
+	if category.FileId == 0 {
+		return errorResponse(ctx, "Отсутствует идентификатор файла обложки")
 	}
 
-	file, err := services.CreateAndSaveFile(image, models.CategoryPath)
+	stringFileId := strconv.FormatUint(uint64(category.FileId), 10)
+	file, err := filesRepository.FindById(stringFileId)
 	if err != nil {
-		return ctx.JSON(err)
+		return errorResponse(ctx, "Файл обложки не найден")
 	}
 
 	category.File = *file
 	validationErrors := validation.Validate(category)
 	if len(validationErrors) >= 1 {
-		return ctx.JSON(fiber.Map{
-			"success": false,
-			"errors":  validationErrors,
-		})
+		return errorResponse(ctx, fiber.Map{"errors": validationErrors})
 	}
 
 	categoriesRepository.Create(category)
-	return ctx.JSON(fiber.Map{
-		"success": true,
-		"message": "Категория успешно создана",
-	})
+	return successResponse(ctx, "Категория успешно создана")
 }
 
 func UpdateCategory(ctx *fiber.Ctx) error {
 	id := ctx.Params("id")
 	category, err := categoriesRepository.FindById(id)
 	if err != nil {
-		return ctx.JSON(fiber.Map{
-			"success": false,
-			"message": err.Error(),
-		})
+		return errorResponse(ctx, "Не найдена категория для обновления")
 	}
+	prevFileId := category.FileId
 
 	if err := ctx.BodyParser(category); err != nil {
-		return ctx.Status(http.StatusBadRequest).JSON(err)
+		return errorResponse(ctx.Status(http.StatusBadRequest), "Неверная структура запроса")
 	}
 
-	image, _ := ctx.FormFile("image")
-	if image != nil {
-		err := services.ReplaceFile(image, models.CategoryPath, &category.File)
+	if category.FileId == 0 {
+		return errorResponse(ctx, "Отсутствует идентификатор файла обложки")
+	}
+
+	if prevFileId != category.FileId {
+		stringFileId := strconv.FormatUint(uint64(category.FileId), 10)
+		file, err := filesRepository.FindById(stringFileId)
 		if err != nil {
-			return ctx.JSON(fiber.Map{
-				"success": false,
-				"message": err.Error(),
-			})
+			return errorResponse(ctx, "Файл обложки не найден")
 		}
+
+		filesRepository.Delete(&category.File)
+		category.File = *file
 	}
 
 	validationErrors := validation.Validate(category)
 	if len(validationErrors) >= 1 {
-		return ctx.JSON(fiber.Map{
-			"success": false,
-			"errors":  validationErrors,
-		})
+		return errorResponse(ctx, fiber.Map{"errors": validationErrors})
 	}
 
 	categoriesRepository.Update(category)
-	return ctx.JSON(fiber.Map{
-		"success": true,
-	})
+	return successResponse(ctx, "Категория успешно обновлена")
 }
 
 func DeleteCategory(ctx *fiber.Ctx) error {
 	id := ctx.Params("id")
 	category, err := categoriesRepository.FindById(id)
 	if err != nil {
-		return ctx.JSON(fiber.Map{
-			"success": false,
-			"message": err.Error(),
-		})
+		return errorResponse(ctx, err.Error())
 	}
+
 	categoriesRepository.Delete(category)
-	return ctx.JSON(fiber.Map{
-		"success": true,
-		"message": "Категория успешно удалена",
-	})
+	return successResponse(ctx, "Категория успешно удалена")
 }
